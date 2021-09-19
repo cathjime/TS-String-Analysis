583ef2311e71ea951ab5b4c6b45afe8aa57f78f8
cjimenez <xcatherine.jimenez@gmail.com>
2021-09-19 10:09:01 -0400

    created variables for expected functionalities

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("I feel refreshed and eager to get back to work and continue to expand my skills.");
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstString = readline_sync_1.question("Enter a word:\n");
+    var charCount = readline_sync_1.question("This word contains n characters:\n");
+    var wordCount = readline_sync_1.question("This word contains n words:\n");
+    var letterDupes = readline_sync_1.question("Letter appears n times:\n");
+    console.log(charCount, wordCount, letterDupes);
+}
+main();
--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
-console.log(
-  "I feel refreshed and eager to get back to work and continue to expand my skills."
-);
+import { question } from "readline-sync";
+
+function main(): void {
+  const firstString: string = question("Enter a word:\n");
+  const charCount: string = question("This word contains n characters:\n");
+  const wordCount: string = question("This word contains n words:\n");
+  const letterDupes: string = question("Letter appears n times:\n");
+  console.log(charCount, wordCount, letterDupes);
+}
+
+main();
