a16ae0cfe476555d046b7c47ef41ac4bc604a31e
cjimenez <xcatherine.jimenez@gmail.com>
2021-09-19 09:55:41 -0400

    installed readline sync package

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("I feel refreshed and eager to get back to work and continue to expand my skills.");
---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log(
+  "I feel refreshed and eager to get back to work and continue to expand my skills."
+);
